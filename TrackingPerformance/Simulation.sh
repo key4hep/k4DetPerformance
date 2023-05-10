314e6b0d978ef3489273e8dcf3d9c93a7d4266f7
Gaelle Sadowski <gasadows@lxplus743.cern.ch>
2023-05-10 15:53:09 +0200

    Addind scripts

---
+++ TrackingPerformance/Simulation.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/bash
+
+# Define a string variable with a value
+ParticleList="mu"  ##"mu e pi"
+MomentumList="0.5 1 2 5 10 20 50 100 200"
+ThetaList="10 30 50 70 89"
+
+# Iterate the string variable using for loop
+for Particle in $ParticleList; do
+    for momentum in $MomentumList; do
+        for theta in $ThetaList     ##{STRAT..END.STEP}
+        do
+            echo "running ddsim with Particle $Particle-, Theta = $theta deg, Momentum = $momentum Gev"
+
+            ddsim --compactFile $LCGEO/FCCee/compact/FCCee_o2_v02/FCCee_o2_v02.xml \
+                        --outputFile /eos/user/g/gasadows/Output/TrackingPerformance/SIM/SIM_$Particle-$theta-deg_$momentum-GeV_1000evt_edm4hep.root \
+                        --steeringFile CLICPerformance/fcceeConfig/fcc_steer.py \
+                        --random.seed 0123456789 \
+                        --enableGun \
+                        --gun.particle $Particle- \
+                        --gun.energy "$momentum*GeV" \
+                        --gun.distribution uniform \
+                        --gun.thetaMin "$theta*deg" \
+                        --gun.thetaMax "$theta*deg" \
+                        --crossingAngleBoost 0 \
+                        --numberOfEvents 1000  \
+                        > /dev/null
+            done    ##loop over theta
+    done    ##loop over momentum
+done  ##loop over particle
